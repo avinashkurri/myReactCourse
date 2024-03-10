609a4572e24dc6e87618b0fb79beaa2f307f666e
Avinash Kurri <avinash.kurri@jadeglobal.com>
2024-03-10 15:39:08 +0530

    Episode4

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 Tag"),
-    React.createElement("h2", {}, "I am h2 Tag"),
-  ]),
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 Tag"),
-    React.createElement("h2", {}, "I am h2 Tag"),
-  ]),
-]);
+const reactelement = React.createElement(
+  "h1",
+  { id: "Heading" },
+  "Welcome Avinash"
+);
+const Element2 = () => {
+  <h1 class="heading" tableIndex="7">
+    {"JSX Heading"}
+  </h1>;
+};
+
+const Element3 = () => {
+  return <h1>This is a functional components element3</h1>;
+};
+
+const Element4 = () => <h1>This is a functional components , short hand4</h1>;
+const Element5 = () => {
+  return <h1>This is a functional components , short hand5</h1>;
+};
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Element2 />
+    {Element3()}
+    <Element4></Element4>
+    <Element5 />
+    <h1>This is compoenent with out return</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
